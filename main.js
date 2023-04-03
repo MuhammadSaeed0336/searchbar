de09ffa0227a467d5fdc7e7f300d3d55232040df
Muhammad Saeed <MuhammadSaeed0336>
2023-04-03 13:06:48 +0500

    animated search bar

---
+++ main.js
@@ -0,0 +1,8 @@
+ const searchBarContainerE1 = document.querySelector('.search-bar-container');
+ const searchContainerE1 = document.querySelector('.search-icon');
+ const micContainerE1 = document.querySelector('.mic-icon');
+
+
+ searchBarContainerE1.addEventListener("click", ()=>{
+    searchBarContainerE1.classList.toggle("active");
+ })
